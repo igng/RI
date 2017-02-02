64dfb89d2ea1ff6a574977bb730f609bfac6705c
igng <monti.d.andrea@gmail.com>
2017-02-02 21:30:20 +0100

    Computer vision

--- MATLAB/Computer_vision/vision.m
+++ MATLAB/Computer_vision/vision.m
@@ -9,32 +9,94 @@ orig = imread('./pock_1.jpg');
 imgsize = size(orig);
 orig_gray = rgb2gray(orig);
 fin_img = imbinarize(orig_gray, 0.45);
-com_img = imcomplement(fin_img);
-figure(1)
+% I = ones(100, 100);
+% I(25:75, 25:75) = 0;
+% fin_img = I;
+fin_img = imcomplement(fin_img);
+figure(100)
 imshow(orig)
-figure(2)
-imshow(com_img)
+figure(200)
+imshow(fin_img)
+center = floor((size(fin_img)+1)/2);
 clc
 
 %% Image processing and pixel count
-% fin_img = zeros(100, 100);
-% fin_img(25:75, 25:75) = 1;
-res = 36;
+res = 15;
 d_angle = 180/res;
 theta = 0:d_angle:180;
-[R, xp] = radon(com_img, theta);
+[R, xp] = radon(fin_img, theta);
 % for i = 1:res + 1
-%     figure(i+10);
-%     plot(xp, R(:,i));
+%     figure(i+1);
+%     plot(xp, R(:, i));
 %     grid on;
 %     str = sprintf('%d degree', theta(i));
 %     title(str);
 % end
+%% Diagonals and center search
 col = zeros(1, 2);
+p = zeros(2, 2*center(2) + 1);
+angle = 0;
+mid_angle = 0;
 for i = 1:2
     [num, idx] = max(R(:));
     [x, y] = ind2sub(size(R), idx);
+    L = xp(x);
     col(1, i) = y;
+    angle = -(y - 1)*d_angle;
+    mid_angle = mid_angle + angle;
+    x_start = center(2) + L*cos(angle*pi/180);
+    y_start = center(1) + L*sin(angle*pi/180);
+    x_end = x_start + 20*L*cos(angle*pi/180 + pi/2);
+    y_end = y_start + 20*L*sin(angle*pi/180 + pi/2);
+%     figure(100 + i);
+%     plot(xp, R(:, y));
+%     grid on;
+%     str = sprintf('%d degree', theta(y));
+%     title(str);
+    figure(200);
+    hold on;
+%     scatter(x_start, y_start, 'filled', 'green');
+%     scatter(x_end, y_end, 'filled', 'blue');
+    x_line = [x_start, x_end];
+    y_line = [y_start, y_end];
+    coeffs = polyfit(x_line, y_line, 1);
+    x_pol = 0:2*center(2);
+    y_pol = polyval(coeffs, x_pol);
+    p(i, :) = y_pol;
+    plot(x_pol, p(i, :), 'r--');
     R(:, y) = 0;
+    hold off;
 end
-col*d_angle
+tollerance = 0;
+c = p(1, :) - p(2, :);
+x_center = find(abs(c) < tollerance);
+while (length(x_center) ~= 1)
+    tollerance = tollerance + 0.1
+    x_center = find(abs(c) < tollerance);
+end
+y_center = y_pol(x_center);
+figure(200);
+hold on;
+scatter(x_center, y_center, 'filled', 'red');
+hold off;
+mid_angle = -floor((mid_angle + d_angle)/2);
+angle_index = floor(mid_angle/d_angle) + 1;
+%% Draw rectangle approximation
+alpha = mid_angle*pi/180;
+w = length(find(R(:, angle_index) > 0));
+h = max(R(:, angle_index));
+x_ver = x_center - cos(-alpha)*w/2 + sin(-alpha)*h/2;
+y_ver = y_center - sin(-alpha)*w/2 - cos(-alpha)*h/2;
+figure(200);
+hold on;
+hold off;
+xv = [0, w, w, 0, 0];
+yv = [0 0 h h 0];
+Q(1, :) = xv;
+Q(2, :) = yv;
+Rm = [cos(-alpha) -sin(-alpha); sin(-alpha) cos(-alpha)];
+XY = Rm*Q;
+figure(200);
+hold on;
+plot(XY(1, :) + x_ver, XY(2, :) + y_ver, 'b');
+hold off;
