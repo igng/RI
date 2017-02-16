c969946e85c3767b4ba43e21b01c61fc324e66dd
igng <monti.d.andrea@gmail.com>
2017-02-16 11:23:14 +0100

    Computer vision [completed]

--- MATLAB/Computer_vision/vision.m
+++ MATLAB/Computer_vision/vision.m
@@ -4,40 +4,52 @@ clc;
 cd /home/igng/Università/'1° Anno'/'1° Semestre'/'Robotica Industriale'/MATLAB/Computer_vision/
 set(0, 'DefaultFigureWindowStyle', 'docked');
 %% Acquisition and noise reduction
-
-orig = imread('./usb.jpg');
+tic
+orig = imread('./pock_1.jpg');
 imgsize = size(orig);
 orig_gray = rgb2gray(orig);
-fin_img = imbinarize(orig_gray, 0.39);
-% I = ones(100, 100);
-% I(25:75, 25:75) = 0;
-% fin_img = I;
+fin_img = imbinarize(orig_gray, 0.43);
 fin_img = imcomplement(fin_img);
-area = bwarea(fin_img);
-figure(100)
+SE = [[0, 1, 0]; [1, 1, 1]; [0, 1, 0]];
+ero_img = imerode(fin_img, SE);
+ero_area = bwarea(ero_img);
+mat_area = bwarea(fin_img);
+mat_perim = mat_area - ero_area;
+figure(1)
 imshow(orig)
-figure(200)
+figure(2)
 imshow(fin_img)
 center = floor((size(fin_img)+1)/2);
 clc
 
 %% Image processing and pixel count
-res = 12;
-min_grid = floor(sqrt(res));
-max_grid = (ceil(sqrt(res)));
+
+res = 36;
+max_grid = 4;
+max_num = max_grid^2;
+offset = 3;
+n_subplot = 0;
 d_angle = 180/res;
 theta = 0:d_angle:180;
 [R, xp] = radon(fin_img, theta);
-% figure(50);
 % for i = 1:res
-%     s = subplot(max_grid, min_grid, i);
+%     m = mod(i, max_num);
+%     if (m == 0)
+%         n_subplot = max_num;
+%         figure(i/max_num + offset - 1);
+%     else
+%         n_subplot = m;
+%         figure(floor(i/max_num) + offset);
+%     end
+%     s = subplot(max_grid, max_grid, n_subplot);
 %     plot(xp, R(:, i));
 %     axis(s, [-1000 1000 0 700]);
 %     grid on;
-%     str = sprintf('%d', theta(i));
+%     str = sprintf('%.2f', theta(i));
 %     title(str);
 % end
 %% Diagonals and center search
+
 col = zeros(1, 2);
 p = zeros(2, 2*center(2) + 1);
 angle = 0;
@@ -47,21 +59,14 @@ for i = 1:2
     [x, y] = ind2sub(size(R), idx);
     L = xp(x);
     col(1, i) = y;
-    angle = -(y - 1)*d_angle
+    angle = -(y - 1)*d_angle;
     mid_angle = mid_angle + angle;
     x_start = center(2) + L*cos(angle*pi/180);
     y_start = center(1) + L*sin(angle*pi/180);
     x_end = x_start + 20*L*cos(angle*pi/180 + pi/2);
     y_end = y_start + 20*L*sin(angle*pi/180 + pi/2);
-%     figure(100 + i);
-%     plot(xp, R(:, y));
-%     grid on;
-%     str = sprintf('%d degree', theta(y));
-%     title(str);
-    figure(200);
+    figure(2);
     hold on;
-%     scatter(x_start, y_start, 'filled', 'green');
-%     scatter(x_end, y_end, 'filled', 'blue');
     x_line = [x_start, x_end];
     y_line = [y_start, y_end];
     coeffs = polyfit(x_line, y_line, 1);
@@ -69,7 +74,7 @@ for i = 1:2
     y_pol = polyval(coeffs, x_pol);
     p(i, :) = y_pol;
     plot(x_pol, p(i, :), 'r--');
-    max(R(:, y))
+    max(R(:, y));
     R(:, y) = 0;
     hold off;
 end
@@ -82,19 +87,20 @@ while (isempty(x_center))
 end
 x_center = x_center(1);
 y_center = y_pol(x_center);
-figure(200);
+figure(2);
 hold on;
 scatter(x_center, y_center, 'filled', 'red');
 hold off;
 mid_angle = -floor((mid_angle + d_angle)/2);
 angle_index = floor(mid_angle/d_angle) + 1;
 %% Draw rectangle approximation
+
 alpha = mid_angle*pi/180;
 w = length(find(R(:, angle_index) > 0));
 h = max(R(:, angle_index));
 x_ver = x_center - cos(-alpha)*w/2 + sin(-alpha)*h/2;
 y_ver = y_center - sin(-alpha)*w/2 - cos(-alpha)*h/2;
-figure(200);
+figure(2);
 hold on;
 hold off;
 xv = [0, w, w, 0, 0];
@@ -103,7 +109,13 @@ Q(1, :) = xv;
 Q(2, :) = yv;
 Rm = [cos(-alpha) -sin(-alpha); sin(-alpha) cos(-alpha)];
 XY = Rm*Q;
-figure(200);
+figure(2);
 hold on;
 plot(XY(1, :) + x_ver, XY(2, :) + y_ver, 'b');
 hold off;
+toc
+%% Area and perimeter calculation
+% fprintf('MATLAB area: %f\n', mat_area);
+% fprintf('MATLAB peri: %f\n', mat_perim);
+% fprintf('My area: %f\n', w*h);
+% fprintf('My peri : %f\n', (w+h)*2);
